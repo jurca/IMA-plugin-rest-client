7470add14cb557e1dd22a29e27aa1887793853ca
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-03-13 08:20:50 +0100

    added abstract implementation of the REST client

---
+++ HttpMethod.js
@@ -0,0 +1,40 @@
+
+/**
+ * Constants representing the upper-case names of supported HTTP methods.
+ */
+export default Object.freeze({
+	/**
+	 * The GET HTTP method.
+	 *
+	 * @type {string}
+	 */
+	GET: "GET",
+
+	/**
+	 * The POST HTTP method.
+	 *
+	 * @type {string}
+	 */
+	POST: "POST",
+
+	/**
+	 * The PATCH HTTP method.
+	 *
+	 * @type {string}
+	 */
+	PATCH: "PATCH",
+
+	/**
+	 * The PUT HTTP method.
+	 *
+	 * @type {string}
+	 */
+	PUT: "PUT",
+
+	/**
+	 * The DELETE HTTP method.
+	 *
+	 * @type {string}
+	 */
+	DELETE: "DELETE"
+});
